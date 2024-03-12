6ea80b6c9917e2288e73b76cdd90b6fb983a72d5
Vy <kvy126@gmail.com>
2024-03-12 16:29:14 +0700

    login success

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,5 +15,15 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtMatKhau.Text == "admin")
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+        }
     }
 }
